b88da99b46e651fa2831d7735ea6014e61f476e6
Gabriel Paiva <gabrielpaiva44@hotmail.com>
2024-08-23 17:42:22 -0300

    Envio de novos arquivos

---
+++ frontend/build/precache-manifest.beb9058ef296bc07eae4dde76ee67077.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6791e698f3b7a816061162a1796b012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e1e5d5504d83f348ad",
+    "url": "/static/css/2.a74afd47.chunk.css"
+  },
+  {
+    "revision": "e6ccf158af2c400b78ef",
+    "url": "/static/css/main.468346f7.chunk.css"
+  },
+  {
+    "revision": "59e1e5d5504d83f348ad",
+    "url": "/static/js/2.5cc446b0.chunk.js"
+  },
+  {
+    "revision": "7599ea5687a2799e30fe665780c781ad",
+    "url": "/static/js/2.5cc446b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2100a27b51f035b4046",
+    "url": "/static/js/3.c738110d.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.c738110d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb7f95aa1246dfbc4a9",
+    "url": "/static/js/4.ed0bd7d5.chunk.js"
+  },
+  {
+    "revision": "b63e781656a7c21988295c1056f07347",
+    "url": "/static/js/4.ed0bd7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292efc6ca1dea309f82e",
+    "url": "/static/js/5.5197de3c.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/5.5197de3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ccf158af2c400b78ef",
+    "url": "/static/js/main.6ee9582c.chunk.js"
+  },
+  {
+    "revision": "11e41ecd668d206ee4c1",
+    "url": "/static/js/runtime-main.3bad45da.js"
+  },
+  {
+    "revision": "9116d6f0bfbc96a4e0edccbd515d11ae",
+    "url": "/static/media/bg.9116d6f0.svg"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "0b6b3c8d2c74fc2e0be8f5d940ec1e14",
+    "url": "/static/media/instagram.79535458.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.55dcf941.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.4c52474f.png"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.bded4f44.png"
+  },
+  {
+    "revision": "920543cc556b25795a7ce3de679d2cc4",
+    "url": "/static/media/wave.437422b1.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.20ba7126.png"
+  },
+  {
+    "revision": "ae8a9a2fbeb676e0a58add382ddea93a",
+    "url": "/static/media/whatsapp.7f02d15e.png"
+  }
+]);
